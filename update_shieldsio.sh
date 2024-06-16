bb4ef94bdb2b86fb9ffb296f370cf8c04585a8f5
haya14busa <haya14busa@gmail.com>
2024-06-16 12:48:39 +0900

    improve shieldio message

--- update_shieldsio.sh
+++ update_shieldsio.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DATA_FILE=$1
 SHIELDS_IO_FILE="${DATA_FILE/data.json/shieldsio.json}"
-cat "${DATA_FILE}" | jq '{schemaVersion: 1, label: "Used-by", message: (.count.total | tostring), namedLogo: "github", color: "brightgreen"}' > "${SHIELDS_IO_FILE}"
+cat "${DATA_FILE}" | jq '{schemaVersion: 1, label: "Used-by", message: ((.count.total | tostring) + " repos"), namedLogo: "github", color: "brightgreen"}' > "${SHIELDS_IO_FILE}"
