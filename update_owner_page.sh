b6b2de12baac3083778a7983ea52e36faf8aa254
haya14busa <haya14busa@gmail.com>
2024-06-16 15:53:38 +0900

    Show top 500 for owner page

--- update_owner_page.sh
+++ update_owner_page.sh
@@ -3,5 +3,5 @@ export OWNER=$1
 
 jq -s '[.[].repos] | add | unique_by(.url) | sort_by(.stars) | reverse' \
   $(find "data/${OWNER}" | grep data.json) |
-  jq '.[:100]' |
+  jq '.[:500]' |
   gomplate -f tmpls/owner.tmpl -d repos=stdin:///in.json > "repo/${OWNER}/README.md"
